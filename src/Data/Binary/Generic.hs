2f41a8eb94cd51e81af7352bd07efd4e8387177b
Lars Petersen <info@lars-petersen.net>
2010-08-18 15:09:25 +0000

    more general approach to extending serialisation

--- src/Data/Binary/Generic.hs
+++ src/Data/Binary/Generic.hs
@@ -9,38 +9,7 @@
 -- Maintainer  : Lars Petersen <info@lars-petersen.net>
 -- Stability   : experimental
 --
--- The following primitive datatypes are used as basecases and get serialized
--- according to their instances of @Data.Binary@:
 -- 
--- - 'Char'
--- 
--- - 'Int'
--- 
--- - 'Word'
--- 
--- - 'Integer'
--- 
--- - 'Int8'
--- 
--- - 'Int16'
--- 
--- - 'Int32'
--- 
--- - 'Int64'
--- 
--- - 'Word8'
--- 
--- - 'Word16'
--- 
--- - 'Word32'
--- 
--- - 'Word64'
--- 
--- - 'Data.ByteString.Lazy.ByteString'
--- 
--- - 'Data.Text.Lazy.Text' encoded as Utf8
--- 
--- 'Float' and 'Double' are serialized according to 'IEEE754'.
 -- For any algebraic datatype just make it an instance of class @Data.Data@
 -- by simply deriving 'Data' on definition or try stand-alone-deriving.
 --
@@ -50,86 +19,52 @@ module Data.Binary.Generic (
     
      getGeneric
    , putGeneric
-   , extend
-   , extendDefault
+
+   , getDefault
+   , putDefault
 
    ) where
 
 import Data.Binary
 import Data.Binary.Put     (putWord16le)
 import Data.Binary.Get     (getWord16le)
-import Data.Binary.IEEE754 (putFloat32be, getFloat32be, putFloat64be, getFloat64be)
+import Data.Binary.Generic.Extensions
 
 import Data.Data
-import Data.Generics
 
 import Data.Word ()
-import Data.Int  
-import Data.ByteString.Lazy (ByteString)  
-import Data.Text.Lazy (Text) 
-import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
 
 
 getGeneric :: (Data a) => Get a
 getGeneric  = generalCase 
-              `extR` (get          :: Get Char)
-              `extR` (get          :: Get Int)
-              `extR` (get          :: Get Word)
-              `extR` (get          :: Get Integer)
-              `extR` (get          :: Get Int8)
-              `extR` (get          :: Get Int16)
-              `extR` (get          :: Get Int32)
-              `extR` (get          :: Get Int64)
-              `extR` (get          :: Get Word8)
-              `extR` (get          :: Get Word16)
-              `extR` (get          :: Get Word32)
-              `extR` (get          :: Get Word64)
-              `extR` (get          :: Get ByteString)
-              `extR` (getText      :: Get Text)
-              `extR` (getFloat32be :: Get Float)
-              `extR` (getFloat64be :: Get Double)
               where
-                getText       = get >>= (return . decodeUtf8)
                 fromIntegralM = return . fromIntegral
                 myDataType    = dataTypeOf ((undefined :: Get b -> b) generalCase)
+                typeName      = showsTypeRep (typeOf $ (undefined :: Get b -> b) generalCase) ""
                 generalCase   = let imax  = maxConstrIndex myDataType
-                                    index | imax == 1     = return 0     :: Get Int
+                                    index | imax == 0     = error $ "getGeneric: `" ++ typeName ++ "' is not algebraic."
+                                          | imax == 1     = return 0     :: Get Int
                                           | imax <= 256   = getWord8    >>= fromIntegralM
                                           | imax <= 65536 = getWord16le >>= fromIntegralM 
-                                          | otherwise     = error "getGeneric: constructor index out of range"
+                                          | otherwise     = error   "getGeneric: constructor index out of range."
                                 in  index >>= \i-> fromConstrM getGeneric (indexConstr myDataType (i+1))
 
 putGeneric  :: (Data a) => a -> Put 
-putGeneric   = generalCase 
-               `extQ` (put              :: Char       -> Put)
-               `extQ` (put              :: Int        -> Put) 
-               `extQ` (put              :: Word       -> Put) 
-               `extQ` (put              :: Integer    -> Put)
-               `extQ` (put              :: Int8       -> Put)
-               `extQ` (put              :: Int16      -> Put)
-               `extQ` (put              :: Int32      -> Put)
-               `extQ` (put              :: Int64      -> Put)
-               `extQ` (put              :: Word8      -> Put)
-               `extQ` (put              :: Word16     -> Put)
-               `extQ` (put              :: Word32     -> Put)
-               `extQ` (put              :: Word64     -> Put)
-               `extQ` (put              :: ByteString -> Put)
-               `extQ` (put . encodeUtf8 :: Text       -> Put)
-               `extQ` (putFloat32be     :: Float      -> Put)
-               `extQ` (putFloat64be     :: Double     -> Put)
-               where
-                 generalCase t = let i        = fromIntegral $ constrIndex (toConstr t) - 1 
-                                     imax     = maxConstrIndex (dataTypeOf t) 
-                                     putIndex | imax == 1     = return      ()
-                                              | imax <= 256   = putWord8     i 
-                                              | imax <= 65536 = putWord16le  i 
-                                              | otherwise     = error "putGeneric: constructor index out of range"
-                                 in  foldl (>>) putIndex (gmapQ putGeneric t) 
+putGeneric t = let i        = fromIntegral $ constrIndex (toConstr t) - 1 
+                   imax     = maxConstrIndex (dataTypeOf t) 
+                   typeName = showsTypeRep (typeOf t) ""
+                   putIndex | imax == 0     = error $ "putGeneric: `" ++ typeName ++ "' is not algebraic."
+                            | imax == 1     = return      ()
+                            | imax <= 256   = putWord8     i 
+                            | imax <= 65536 = putWord16le  i 
+                            | otherwise     = error   "putGeneric: constructor index out of range."
+               in  foldl (>>) putIndex (gmapQ putGeneric t) 
   
+defaultExtended :: (Data a) => (Get a, a -> Put)
+defaultExtended  = defaultExtension (getGeneric, putGeneric)
 
-extend :: (Typeable a, Typeable b) => (Get a, a -> Put) -> (Get b, b -> Put) -> (Get a, a -> Put)
-extend (g,p) (g',p') = (g `extR` g', p `extQ` p')
+getDefault :: (Data a) => Get a
+getDefault  = fst defaultExtended
 
-extendDefault :: (Data a, Typeable b) => (Get b, b -> Put) -> (Get a, a -> Put)
-extendDefault  = extend (getGeneric, putGeneric)
-
+putDefault :: (Data a) => a -> Put
+putDefault  = snd defaultExtended
---
+++ src/Data/Binary/Generic/Extensions.hs
@@ -0,0 +1,139 @@
+{-# OPTIONS -XNoMonomorphismRestriction #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Data.Binary.Generic.Extensions
+-- Copyright   : Lars Petersen
+-- License     : BSD3-style (see LICENSE)
+-- 
+-- Maintainer  : Lars Petersen <info@lars-petersen.net>
+-- Stability   : experimental
+--
+-- The following primitive datatypes are used as basecases and get serialized
+-- according to their instances of @Data.Binary@:
+-- 
+-- - 'Char'
+-- 
+-- - 'Int'
+-- 
+-- - 'Word'
+-- 
+-- - 'Integer'
+-- 
+-- - 'Int8'
+-- 
+-- - 'Int16'
+-- 
+-- - 'Int32'
+-- 
+-- - 'Int64'
+-- 
+-- - 'Word8'
+-- 
+-- - 'Word16'
+-- 
+-- - 'Word32'
+-- 
+-- - 'Word64'
+-- 
+-- - 'Data.ByteString.Lazy.ByteString'
+-- 
+-- - 'Data.Text.Lazy.Text' encoded as Utf8
+-- 
+-- 'Float' and 'Double' are serialized according to 'IEEE754'.
+-- For any algebraic datatype just make it an instance of class @Data.Data@
+-- by simply deriving 'Data' on definition or try stand-alone-deriving.
+--
+-----------------------------------------------------------------------------
+
+module Data.Binary.Generic.Extensions (
+    
+     extGet
+   , extPut
+
+   , defaultExtension
+
+   , charExt
+   , intExt
+   , wordExt
+   , floatExt
+   , byteStringExt
+   , textExt
+
+   ) where
+
+import Data.Binary
+import Data.Binary.IEEE754 (putFloat32be, getFloat32be, putFloat64be, getFloat64be)
+
+import Data.Data
+import Data.Generics
+
+import Data.Word ()
+import Data.Int  
+import Data.ByteString.Lazy (ByteString)  
+import Data.Text.Lazy (Text) 
+import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8)
+
+type Extension a = (Get a, a -> Put) -> (Get a, a -> Put)
+
+extGet = extR
+extPut = extQ
+
+defaultExtension :: (Typeable a) => Extension a
+defaultExtension  =   byteStringExt 
+                    . textExt 
+                    . floatExt 
+                    . wordExt 
+                    . intExt 
+                    . charExt
+
+charExt      :: (Typeable a) => Extension a
+charExt (g,p) = let g' = g `extGet` (get          :: Get Char         )
+                    p' = p `extPut` (put          :: Char       -> Put)
+                in (g',p')
+
+wordExt      :: (Typeable a) => Extension a
+wordExt (g,p) = let g' = g `extGet` (get          :: Get Word         ) 
+                           `extGet` (get          :: Get Word8        )
+                           `extGet` (get          :: Get Word16       )
+                           `extGet` (get          :: Get Word32       )
+                           `extGet` (get          :: Get Word64       )
+                    p' = p `extPut` (put          :: Word       -> Put)
+                           `extPut` (put          :: Word8      -> Put)
+                           `extPut` (put          :: Word16     -> Put)
+                           `extPut` (put          :: Word32     -> Put)
+                           `extPut` (put          :: Word64     -> Put)
+                in (g',p')
+
+intExt       :: (Typeable a) => Extension a
+intExt  (g,p) = let g' = g `extGet` (get          :: Get Int          )  
+                           `extGet` (get          :: Get Int8         )
+                           `extGet` (get          :: Get Int16        )
+                           `extGet` (get          :: Get Int32        )
+                           `extGet` (get          :: Get Int64        )
+                    p' = p `extPut` (put          :: Int        -> Put)
+                           `extPut` (put          :: Int8       -> Put)
+                           `extPut` (put          :: Int16      -> Put)
+                           `extPut` (put          :: Int32      -> Put)
+                           `extPut` (put          :: Int64      -> Put)
+                in (g',p')
+
+byteStringExt :: (Typeable a) => Extension a
+byteStringExt (g,p) = let g' = g `extGet` (get    :: Get ByteString   )
+                          p' = p `extPut` (put    :: ByteString -> Put)
+                      in (g',p')
+
+textExt       :: (Typeable a) => Extension a
+textExt  (g,p) = let getText = get >>= (return . decodeUtf8)
+                     putText = put             . encodeUtf8
+                     g'      = g `extGet` (getText :: Get Text        )
+                     p'      = p `extPut` (putText :: Text      -> Put)
+                 in (g',p')
+
+floatExt      :: (Typeable a) => Extension a
+floatExt (g,p) = let g' = g `extGet` (getFloat32be :: Get Float       )  
+                            `extGet` (getFloat64be :: Get Double      )
+                     p' = p `extPut` (putFloat32be :: Float     -> Put)
+                            `extPut` (putFloat64be :: Double    -> Put)
+                 in (g',p')
+
